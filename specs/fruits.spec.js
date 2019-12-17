ba1428671d3b20ed2c1d3f5833cf58edc1005301
AninHuang <48175930+AninHuang@users.noreply.github.com>
2019-12-17 16:28:23 +0800

    Traversing The DOM

--- specs/fruits.spec.js
+++ specs/fruits.spec.js
@@ -1,2 +1,21 @@
 import  FruitBasket from '@/fruit-basket'
 import { mount } from '@vue/test-utils'
+
+test('we can add fruits to basket with DOM', () => {
+    const wrapper = mount(FruitBasket)
+
+    const input = wrapper.find('input')
+    const button = wrapper.find('button')
+    expect(wrapper.findAll('li').length).toBe(0)
+
+    // The test would be fail if not trigger input event because we just set value directly
+    // but Vue uses v-model listening to an input event
+    input.element.value = 'banana'
+    input.trigger('input')
+    expect(wrapper.vm.fruit).toBe('banana')
+
+    button.trigger('click')
+    expect(wrapper.vm.fruit).toBe('')
+    expect(wrapper.vm.basket).toEqual(expect.arrayContaining(['banana']))
+    // expect(wrapper.findAll('li').length).toBe(1) //failed
+});
