c37fc724ec8273bb7174c917b25df83b9a0ff3e4
AninHuang <48175930+AninHuang@users.noreply.github.com>
2020-01-10 13:44:15 +0800

    Use stubs to replace child components

--- specs/stubs.spec.js
+++ specs/stubs.spec.js
@@ -1,1 +1,11 @@
+import { shallowMount } from '@vue/test-utils';
+import ListComponent from '@/list';
 
+test('shallowMount', () => {
+    const wrapper = shallowMount(ListComponent, {
+        stubs: {
+            ListItem: `<div class="list-item" />`
+        }
+    });
+    expect(wrapper).toMatchSnapshot();
+});
